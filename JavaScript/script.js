09974fd3bbbc25a1b59270a1cde26e87725f9131
diegogit2 <diegojeftha18@gmail.com>
2023-04-13 14:25:22 +0200

    upadated and completed challenge IWA10

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -53,63 +53,65 @@ const futureId = 9
 
 // Do not change code above this comment
 
-// console.log(holidays.name || `ID ${futureId} not created yet`)
+console.log(holidays.name || `ID ${futureId} not created yet`)
+ 
+copied = holidays.christmas
 
-// copied = holidays.christmas
-// copied = { 
-//     name: 'X-mas', 
-//     date: new Date(`25 December ${currentYear}`),
-//     id: holidays[christmas].id,
-// }
+copied = { 
+    name: 'X-mas', 
+    date: new Date(`25 December ${currentYear}`),
+    id: holidays[christmas].id,
+}
 
-// correctDate = copied.date
-// correctDate.setHours(0)
-// correctDate.setMinutes(0)
-// let isEarlier = copied.date < holidays['6'].date
+correctDate = copied.date
+correctDate.setHours(0)      
+correctDate.setMinutes(0)
+let isEarlier = copied.date < holidays['6'].date
 
-// console.log('New date is earlier:', isEarlier)
+console.log('New date is earlier:', isEarlier)
 
-// if (isEarlier) {
-//     copied.date = correctDate
-// }
+if (isEarlier) {
+    copied.date = correctDate
+}
 
-// console.log('ID change:', holidays[christmas].id != copied.id && copied.id)
-// console.log('Name change:', holidays[christmas].name === copied.name || copied.name)
-// console.log('Date change:', holidays[christmas].date === copied.date || copied.date.toLocaleDateString())
+console.log('ID change:', holidays[christmas].id != copied.id )
+console.log('Name change:', holidays[christmas].name === copied.name || copied.name)
+console.log('Date change:', holidays[christmas].date === copied.date || copied.date.toLocaleDateString())
 
 holidays[3].date = new Date (`1 January ${currentYear}`)
 
 const firstHolidayTimestamp = Math.min(
-    holidays[0].date,
-    holidays[1].date,
-    holidays[2].date,
-    holidays[3].date,
-    holidays[4].date,
-    holidays[5].date,
-    holidays[6].date,
-    holidays[7].date,
-    holidays[8].date,
+   new Date (holidays[0].date).getTime(),
+   new Date (holidays[1].date).getTime(),
+   new Date (holidays[2].date).getTime(),
+   new Date (holidays[3].date).getTime(),
+   new Date (holidays[4].date).getTime(),
+   new Date (holidays[5].date).getTime(),
+   new Date (holidays[6].date).getTime(),
+   new Date (holidays[7].date).getTime(),
+   new Date (holidays[8].date).getTime(),
 )
 
 const lastHolidayTimestamp = Math.max(
-    holidays[0].date,
-    holidays[1].date,
-    holidays[2].date,
-    holidays[3].date,
-    holidays[4].date,
-    holidays[5].date,
-    holidays[6].date,
-    holidays[7].date,
-    holidays[8].date,
+   new Date (holidays[0].date).getTime(),
+   new Date (holidays[1].date).getTime(),
+   new Date (holidays[2].date).getTime(),
+   new Date (holidays[3].date).getTime(),
+   new Date (holidays[4].date).getTime(),
+   new Date (holidays[5].date).getTime(),
+   new Date (holidays[6].date).getTime(),
+   new Date (holidays[7].date).getTime(),
+   new Date (holidays[8].date).getTime(),
 )
 
-const firstDay = new Date (firstHolidayTimestamp).getDate()
-const firstMonth = new Date (firstHolidayTimestamp).getMonth()
-const lastDay = new Date (lastHolidayTimestamp).getDate()
-const lastMonth = new Date (lastHolidayTimestamp).getMonth()
+console.log(firstHolidayTimestamp)
+let firstDay = new Date(firstHolidayTimestamp).getDate().toString();
+let firstMonth =new Date(firstHolidayTimestamp).getMonth().toString() + 1;
+let lastDay =new Date (lastHolidayTimestamp).getDate();
+let lastMonth =new Date(lastHolidayTimestamp).getMonth() + 1;
 
-console.log(`${firstDay}/${firstMonth }/${currentYear}`)
-console.log(`${lastDay}/${lastMonth }/${currentYear}`)
+console.log (`${firstDay.padStart(2, '0')}/${firstMonth}/${currentYear}`)
+console.log (`${lastDay}/${lastMonth}/${currentYear}`)
 
-const randomHoliday = holidays[Math.random]
-console.log(randomHoliday)
+const randomHoliday = holidays[Math.floor(Math.random() * 9)]
+console.log(randomHoliday.date.toLocaleDateString())
